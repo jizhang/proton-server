28080c1962428de5d32823b466499f48c17991fb
Jerry <zhangji87@gmail.com>
2023-01-03 15:40:05 +0800

    Init

---
+++ src/test/java/com/shzhangji/quark/QuarkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shzhangji.quark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuarkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
